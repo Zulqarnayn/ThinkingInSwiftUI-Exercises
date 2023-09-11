f31f97148beb7e1c82779a064e05b18545296b57
Asif Mujtaba <asif.mujtaba@tigerit.com>
2023-09-11 19:12:34 +0600

    Initial Commit

---
+++ Exercise.1/Exercise_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Exercise_1App.swift
+//  Exercise.1
+//
+//  Created by Asif Mujtaba on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Exercise_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
